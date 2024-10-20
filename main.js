7e97fc4cb1f7cea8c37fa823001dd20a7211de69
mohammedateeef <atefm.me556@gmail.com>
2024-10-20 16:21:52 +0300

    changes

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 // Define the root route
 app.get("/", (req, res) => {
